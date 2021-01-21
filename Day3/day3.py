6b5c904c14421a20abc13ceb2a285a91ac95ec05
navneethsrsh <navneethsrsh90@gmail.com>
2021-01-22 01:09:19 +0530

    Solution Complete

--- Day3/day3.py
+++ Day3/day3.py
@@ -9,46 +9,35 @@ max_height = len(route_map)
 # Part 1
 h = 0
 w = 0
-extend = 1
-trees = 0
-traverse = [path for path in route_map]
+trees= 0
 
 while h < max_height - 1:
-    if w + 3 >= max_width:
-        extend += 1
-        traverse = [path * extend for path in route_map]
-        
-    if traverse[h + 1][w + 3] == '#':
-        trees += 1
-    w  += 3
+    w += 3
     h += 1
+    w = w - max_width if w >= max_width else w       
+    trees += (1 if route_map[h][w] == '#' else 0)
 
 print(trees)
 
 # Part 2
-slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
-mul_trees = 1
 max_width = len(route_map[0])
 max_height = len(route_map)
+# slope --> (moves right, moves down) ---> (x, y)
+slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)] 
+mul_trees = 1
 
 for x, y in slopes:
     h = 0
     w = 0
     trees = 0
-    extend = 1
-    traverse = [path for path in route_map]
-    print(x,y)
+    
     while h < max_height - y:
-        if w + x >= max_width:
-            extend += 1
-            traverse = [path * extend for path in route_map]
-            
-        if traverse[h + y][w + x] == '#':
-            trees += 1
-        w  += x
+        w += x
         h += y
+        w = w - max_width if w >= max_width else w
+        trees += (1 if route_map[h][w] == '#' else 0)            
     mul_trees *= trees
 
-print(trees)
+print(mul_trees)
 
 
